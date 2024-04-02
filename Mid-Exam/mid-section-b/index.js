131a6ba8866124fab49ee3280ca8d2f395539ac6
M Rashid <rashidshar@gmail.com>
2024-04-02 14:55:48 +0500

    mid exam

---
+++ Mid-Exam/mid-section-b/index.js
@@ -0,0 +1,11 @@
+const nextbtn =document.getElementById("next");
+
+const images =document.getElementsByTagName("img");
+
+nextbtn.addEventListener(onclick,nextimg =() =>{
+
+    const img = document.getElementById("imgg");
+    img.setAttribute("src","./three.svg")
+    
+}) 
+
